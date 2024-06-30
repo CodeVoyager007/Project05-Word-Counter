61576822904f31f15c550885e1ca684d97c70376
CodeVoyager007 <162779681+CodeVoyager007@users.noreply.github.com>
2024-06-30 19:18:40 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,24 @@
+#!/user/bin/env node
+//Import the inquirer modulw which is a command line interface for Node.js & chalks for colors
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.red("========================================="));
+console.log(chalk.greenBright("    Welcome To Word Counter    "));
+console.log(chalk.red("========================================="));
+const answer: {
+  Sentence: string;
+} = await inquirer.prompt([
+  {
+    name: "Sentence",
+    type: "input",
+    message: "Enter your sentence to count the words:",
+  },
+]);
+const words = answer.Sentence.trim().split(" ");
+
+//print the arry on the console
+console.log(words);
+
+// prinnt the word coun of the sentence on the console
+
+console.log(`Your sentence word count is ${words.length}`);
